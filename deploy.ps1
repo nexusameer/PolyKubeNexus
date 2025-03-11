8ccf114e9b403d24b58a05c50ddf6854da2b13f9
nexusameer <ameerk10fw@gmail.com>
2025-03-11 15:55:51 +0500

    tech web deploy successfuly

--- deploy.ps1
+++ deploy.ps1
@@ -1,9 +1,11 @@
-kubectl apply -f devsearch-deployment.yaml
 kubectl apply -f portfolio-deployment.yaml
-kubectl apply -f devsearch-service.yaml
 kubectl apply -f portfolio-service.yaml
+kubectl apply -f portfolio-secrets.yaml
+kubectl apply -f devsearch-service.yaml
+kubectl apply -f devsearch-deployment.yaml
+kubectl apply -f techweb-service.yaml
+kubectl apply -f techweb-deployment.yaml
 kubectl apply -f ingress.yaml
-kubectl apply -f secrets.yaml
 kubectl get svc
 kubectl get pods
 kubectl get ingress
