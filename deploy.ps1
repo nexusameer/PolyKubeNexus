3560e6e5b841032d91af95118a0e42d668234afe
nexusameer <ameerk10fw@gmail.com>
2025-03-11 17:08:51 +0500

    add myblog gym studybud

--- deploy.ps1
+++ deploy.ps1
@@ -5,7 +5,16 @@ kubectl apply -f devsearch-service.yaml
 kubectl apply -f devsearch-deployment.yaml
 kubectl apply -f techweb-service.yaml
 kubectl apply -f techweb-deployment.yaml
+kubectl apply -f gym-deployment.yaml
+kubectl apply -f gym-service.yaml
+kubectl apply -f myblog-deployment.yaml
+kubectl apply -f myblog-service.yaml
+kubectl apply -f studybud-deployment.yaml
+kubectl apply -f studybud-service.yaml
+
+
 kubectl apply -f ingress.yaml
+
 kubectl get svc
 kubectl get pods
 kubectl get ingress
